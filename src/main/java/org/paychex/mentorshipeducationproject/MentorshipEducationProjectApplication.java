e7593fe440337e976b279009c8e054be73be22e6
abhi-sharma901 <abhinavsharma2001jk77@gmail.com>
2024-01-20 17:31:56 +0530

    Initial Commit

---
+++ src/main/java/org/paychex/mentorshipeducationproject/MentorshipEducationProjectApplication.java
@@ -0,0 +1,13 @@
+package org.paychex.mentorshipeducationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorshipEducationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorshipEducationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/paychex/mentorshipeducationproject/MentorshipEducationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.paychex.mentorshipeducationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorshipEducationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
